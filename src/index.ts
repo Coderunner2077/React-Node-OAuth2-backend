361a9a8b911f9c2e1559def3276231614c96cb32
TyranReacts <s.mossa1990@gmail.com>
2021-08-06 09:50:51 +0200

    Added middlewares for express + express-session + passport

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
 
 dotenv.config();
 
@@ -13,6 +16,17 @@ connection.once("open", () =>{
 });
 
 const app = express();
+
+// 1. Middlewares
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
     res.send("Welcome to React Node oAuth App");
